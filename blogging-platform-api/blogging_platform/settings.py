1d527da431e65e109fc19751f2089280d56e161f
Agent1c <medicenyb@gmail.com>
2025-01-12 15:27:25 +0200

    Deploy to Heroku

--- blogging-platform-api/blogging_platform/settings.py
+++ blogging-platform-api/blogging_platform/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/4.x/ref/settings/
 
 import os
 from pathlib import Path
-# from django.contrib.auth.models import User
-# from django.db import models
-
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
