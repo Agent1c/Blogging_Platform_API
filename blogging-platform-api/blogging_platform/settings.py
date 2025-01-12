b977a375dcc723699799ab1b8edbf61206464a63
Agent1c <medicenyb@gmail.com>
2025-01-12 12:22:06 +0200

    read for deploy

--- blogging-platform-api/blogging_platform/settings.py
+++ blogging-platform-api/blogging_platform/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-v^gh&)!7c6-nu4bs#o^3dbxgw5t+n3qs^1ze_%!a#53el*2j*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -70,8 +70,13 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'  # Redirect to the login page after logout
 
+MEDIA_ROOT = BASE_DIR/ 'media'
+STATIC_ROOT = BASE_DIR / 'assets'
+
+STATICFILES_DIRS = [BASE_DIR/ 'static']
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR/ 'static']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -86,7 +91,7 @@ ROOT_URLCONF = 'blogging_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
